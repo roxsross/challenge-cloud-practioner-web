6fce85fff7e224290b5023b314d758e61e6b4a76
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2025-09-27 21:13:22 -0300

    Update install.sh

--- script-example/nginx/amazon-linux/install.sh
+++ script-example/nginx/amazon-linux/install.sh
@@ -3,7 +3,7 @@
 # User Data para Amazon Linux 2023
 sudo yum update -y
 # Instalar nginx y herramientas
-sudo yum install nginx unzip wget -y
+sudo yum install nginx unzip git wget -y
 # Iniciar nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
